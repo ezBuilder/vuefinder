b380c8bb76ea6844d857ea1fe39adef477a3e561
ezBuilder <han314@nate.com>
2022-10-15 11:02:02 +0900

    새로운 빌드

---
+++ dist/tr.2d994891.js
@@ -0,0 +1,71 @@
+const e = `{\r
+  "Language": "Dil",\r
+  "Create": "Olu\u015Ftur",\r
+  "Close": "Kapat",\r
+  "Cancel": "\u0130ptal",\r
+  "Save": "Kaydet",\r
+  "Edit": "D\xFCzenle",\r
+  "Crop": "Kes",\r
+  "New Folder": "Yeni Klas\xF6r",\r
+  "New File": "Yeni Dosya",\r
+  "Rename": "Yeniden Adland\u0131r",\r
+  "Delete": "Sil",\r
+  "Upload": "Kar\u015F\u0131ya Y\xFCkle",\r
+  "Download": "\u0130ndir",\r
+  "Archive": "Ar\u015Fivle",\r
+  "Unarchive": "Ar\u015Fivden \xE7\u0131kar",\r
+  "Open": "A\xE7",\r
+  "Open containing folder": "\u0130\xE7eren klas\xF6r\xFC a\xE7",\r
+  "Refresh": "Yenile",\r
+  "Preview": "\xD6nizleme",\r
+  "Dark Mode": "Karanl\u0131k Mod",\r
+  "Toggle Full Screen": "Tam ekran",\r
+  "Change View": "G\xF6r\xFCn\xFCm\xFC de\u011Fi\u015Ftir",\r
+  "Storage": "Depo",\r
+  "Go up a directory": "Yukar\u0131 git",\r
+  "Search anything..": "Herhangi bir \u015Fey ara..",\r
+  "Name": "Ad",\r
+  "Size": "Boyut",\r
+  "Date": "Tarih",\r
+  "Filepath": "Dosya yolu",\r
+  "About": "Hakk\u0131nda",\r
+  "Folder Name": "Klas\xF6r Ad\u0131",\r
+  "File Name": "Dosya Ad\u0131",\r
+  "Move files": "Dosyalar\u0131 ta\u015F\u0131",\r
+  "Are you sure you want to move these files to?": "Bu dosyalar\u0131 ta\u015F\u0131mak istedi\u011Finizden emin misiniz?",\r
+  "Yes, Move!": "Evet, Ta\u015F\u0131!",\r
+  "Delete files": "Dosyalar\u0131 Sil",\r
+  "Yes, Delete!": "Evet, Sil!",\r
+  "Upload files": "Dosyalar\u0131 Y\xFCkle",\r
+  "No files selected!": "Dosya Se\xE7ilmedi!",\r
+  "Select Files": "Dosyalar\u0131 Se\xE7",\r
+  "Archive the files": "Dosyalar\u0131 ar\u015Fivleyin",\r
+  "Unarchive the files": "Dosyalar\u0131 ar\u015Fivden \xE7\u0131kar\u0131n",\r
+  "The archive will be unarchived at": "Ar\u015Fiv buraya \xE7\u0131kar\u0131lacak: ",\r
+  "Archive name. (.zip file will be created)": "Archive ad\u0131. (.zip dosyas\u0131 olu\u015Fturulacak)",\r
+  "Vuefinder is a file manager component for vue 3.": "Vuefinder, vue 3 i\xE7in bir web dosya y\xF6netim bile\u015Fenidir.",\r
+  "Create a new folder": "Yeni bir klas\xF6r olu\u015Fturun",\r
+  "Create a new file": "Yeni bir dosya olu\u015Fturun",\r
+  "Are you sure you want to delete these files?": "Bu dosyalar\u0131 silmek istedi\u011Finize emin misiniz?",\r
+  "This action cannot be undone.": "Bu i\u015Flem geri al\u0131namaz.",\r
+  "Search results for": "Arama sonu\xE7lar\u0131:",\r
+  "item(s) selected.": "dosya se\xE7ildi.",\r
+  "%s is renamed." : "%s yeniden adland\u0131r\u0131lm\u0131\u015Ft\u0131r.",\r
+  "This is a readonly storage." : "Bu salt okunur bir depolama alan\u0131d\u0131r.",\r
+  "%s is created." : "%s ba\u015Far\u0131yla olu\u015Fturulmu\u015Ftur.",\r
+  "Files moved." : "Dosyalar ta\u015F\u0131nd\u0131.",\r
+  "Files deleted." : "Dosyalar silindi.",\r
+  "The file is unarchived." : "Dosya ar\u015Fivden \xE7\u0131kar\u0131ld\u0131.",\r
+  "The file(s) is archived." : "Dosyalar ar\u015Fivlendi.",\r
+  "Updated." : "G\xFCncellendi.",\r
+  "No search result found." : "Arama sonucu bulunamad\u0131.",\r
+  "Are you sure you want to move these files?" : "Bu dosyalar\u0131 ta\u015F\u0131mak istedi\u011Finize emin misiniz?",\r
+  "File Size": "Dosya Boyutu",\r
+  "Last Modified": "Son De\u011Fi\u015Fiklik",\r
+  "Drag&Drop: on": "S\xFCr\xFCkle&B\u0131rak: etkin",\r
+  "Drag&Drop: off": "S\xFCr\xFCkle&B\u0131rak: devre d\u0131\u015F\u0131"\r
+}\r
+`;
+export {
+  e as default
+};
